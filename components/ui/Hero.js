16350fbb97c1f566bce59491e4213ab2364ce7d7
Ricardo <i.s.ricardo.sandoval@gmail.com>
2021-10-18 07:36:04 -0500

    Image component updated

--- components/ui/Hero.js
+++ components/ui/Hero.js
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function Hero() {
     return (
         <div className="hero">
-            <Image alt={'halloween banner'} src={'/img/halloween.png'} layout='fill' objectFit className='img' ></Image>
+            <Image alt={'halloween banner'} src={'/img/halloween.png'} layout='fill' objectFit className='img' priority></Image>
         </div>
 
     )
